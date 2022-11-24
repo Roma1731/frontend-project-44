c3150c6841d35098e4c5ffd23989798e6508d464
Roma1371 <antonfrolov1212@gmail.com>
2022-11-24 10:46:50 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,15 +4,9 @@ import getRandomNumber from '../Random.js';
 const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  const sqrtnumber = Math.floor(Math.sqrt(number));
-  let prime = number !== 1;
-  for (let i = 2; i < sqrtnumber + 1; i += 1) {
-    if (number % i === 0) {
-      prime = false;
-      break;
-    }
-  }
-  return prime;
+  for (let i = 2, s = Math.sqrt(number); i <= s; i += 1)
+   if (number % i === 0) return false;
+  return number > 1;
 };
 
 const getData = () => {
